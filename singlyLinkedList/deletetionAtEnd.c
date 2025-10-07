1b5abc2653032a6d20bec4549beb46e61e04116a
Kumail Ahmad <kumailahmad217@gmail.com>
2025-10-07 12:00:00 +0530

    singlyLinkedList - DeletionAtEnd

---
+++ singlyLinkedList/deletetionAtEnd.c
@@ -0,0 +1,100 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node
+{
+    int data;
+    struct Node *next;
+};
+
+struct Node *createNode(int val)
+{
+    struct Node *newNode = (struct Node *)malloc(sizeof(struct Node));
+    newNode->data = val;
+    newNode->next = NULL;
+    return newNode;
+};
+
+struct Node *createList(int n)
+{
+    struct Node *head = NULL;
+    struct Node *temp = NULL;
+    struct Node *newNode;
+    int Node;
+    for (int i = 0; i < n; i++)
+    {
+        printf("Enter the data for Node no %d\n", i + 1);
+        scanf("%d", &Node);
+
+        newNode = createNode(Node);
+
+        if (head == NULL)
+        {
+            head = newNode;
+            newNode->next = NULL;
+            temp = head;
+        }
+        else
+        {
+            temp->next = newNode;
+            temp = newNode;
+        }
+    }
+    return head;
+};
+struct Node *insertAtbegining(struct Node *head)
+{
+    int val;
+    printf("Enter the value to be inserted in beginning: \n");
+    scanf("%d", &val);
+
+    struct Node *newNode = createNode(val);
+    newNode->next = head;
+    head = newNode;
+    return head;
+};
+
+struct Node *deletionAtEnd(struct Node *head)
+{
+    struct Node *temp = head;
+    struct Node *prev = NULL;
+    while (temp->next != NULL)
+    {
+        prev = temp;
+        temp = temp->next;
+    }
+    prev->next = NULL;
+    printf("\nLast Node deleted with value %d \n", temp->data);
+    free(temp);
+    return head;
+}
+void display(struct Node *head)
+{
+    printf("List : ");
+    struct Node *temp = head;
+    while (temp != NULL)
+    {
+        printf("%d -> ", temp->data);
+        temp = temp->next;
+    }
+    printf("NULL");
+};
+
+int main()
+{
+    int n, valBeg;
+    struct Node *head = NULL;
+    printf("Enter the no of nodes: ");
+    scanf("%d", &n);
+
+    head = createList(n);
+
+    printf("List Created Succesfully !! \n ");
+
+    // head = insertAtbegining(head);
+    display(head);
+    head = deletionAtEnd(head);
+    display(head);
+
+    return 0;
+}

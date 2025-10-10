ab8299bed426e7e6b3ba5de6cb17c1e22af94127
Kumail Ahmad <kumailahmad217@gmail.com>
2025-10-10 12:00:00 +0530

    CircularLinkedList - insertAtAnyPos

---
+++ singlyCircularLinkedList/insertionAtEnd.c
@@ -0,0 +1,130 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node
+{
+    int data;
+    struct Node *next;
+};
+
+struct Node *createNode(int val)
+{
+    struct Node *newNode = (struct Node *)malloc(sizeof(struct Node));
+    newNode->data = val;
+    newNode->next = NULL;
+    return newNode;
+}
+
+struct Node *createList(int n)
+{
+    struct Node *head = NULL;
+    struct Node *temp = NULL;
+    int nodeElem;
+    for (int i = 0; i < n; i++)
+    {
+        printf("Enter the data for Node no %d: ", i + 1);
+        scanf("%d", &nodeElem);
+
+        struct Node *newNode = createNode(nodeElem);
+
+        if (head == NULL)
+        {
+            head = newNode;
+
+            temp = head;
+        }
+        else
+        {
+            temp->next = newNode;
+            temp = newNode;
+        }
+    }
+    if (temp != NULL)
+    {
+        temp->next = head;
+    }
+
+    return head;
+}
+
+struct Node *insertAtBeginning(struct Node *head)
+{
+
+    int val;
+    printf("Enter the value which you want to insert: ");
+    scanf("%d", &val);
+
+    struct Node *temp = head;
+    struct Node *newNode = createNode(val);
+    if (head == NULL)
+    {
+        newNode->next = head;
+        head = newNode;
+        return head;
+    }
+
+    while (temp->next != head)
+    {
+        temp = temp->next;
+    }
+    newNode->next = head;
+    temp->next = newNode;
+    head = newNode;
+
+    return head;
+}
+
+struct Node *insertAtEnd(struct Node *head)
+{
+    int val;
+    printf("Enter the value which you want to insert: ");
+    scanf("%d", &val);
+
+    struct Node *newNode = createNode(val);
+    if (head == NULL)
+    {
+        head = newNode;
+        head->next = head;
+        return head;
+    }
+    struct Node *temp = head;
+    while (temp->next != head)
+    {
+        temp = temp->next;
+    }
+    temp->next = newNode;
+
+    newNode->next = head;
+    return head;
+}
+
+void display(struct Node *head)
+{
+    if (head == NULL)
+    {
+        printf("Linked List is empty\n");
+    }
+
+    struct Node *temp = head;
+    do
+    {
+        printf("%d -> ", temp->data);
+        temp = temp->next;
+    } while (temp != head);
+    printf("(Back to head)\n");
+}
+
+int main()
+{
+    int n;
+    struct Node *head = NULL;
+    printf("Enter the Number of Nodes: ");
+    scanf("%d", &n);
+
+    head = createList(n);
+
+    head = insertAtEnd(head);
+    display(head);
+
+    return 0;
+}

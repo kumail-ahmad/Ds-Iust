7bb7ba552bfd5c4d6e6583c69ad8a8451e2714b5
Kumail Ahmad <kumailahmad217@gmail.com>
2025-10-11 12:00:00 +0530

    DoublyCircularLinkedList - deletionAtbeginning

---
+++ doublyCircularLinkedList/deletionAtbeginning.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node *next;
+    struct Node *prev;
+};
+
+struct Node *createNode(int val) {
+    struct Node *newNode = (struct Node *)malloc(sizeof(struct Node));
+    newNode->data = val;
+    newNode->next = newNode->prev = NULL;
+    return newNode;
+}
+
+struct Node *createList(int n) {
+    if (n == 0) return NULL;
+    struct Node *head = NULL, *tail = NULL;
+    int val;
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &val);
+        struct Node *node = createNode(val);
+        if (!head) head = tail = node;
+        else {
+            tail->next = node;
+            node->prev = tail;
+            tail = node;
+        }
+    }
+    head->prev = tail;
+    tail->next = head;
+    return head;
+}
+
+struct Node *insertAtBeginning(struct Node *head) {
+    int val;
+    scanf("%d", &val);
+    struct Node *node = createNode(val);
+    if (!head) { node->next = node->prev = node; return node; }
+    struct Node *tail = head->prev;
+    node->next = head;
+    node->prev = tail;
+    tail->next = head->prev = node;
+    return node;
+}
+
+
+
+struct Node *deleteAtBeginning(struct Node *head) {
+    if (!head) return NULL;
+    if (head->next == head) { free(head); return NULL; }
+    struct Node *tail = head->prev;
+    struct Node *temp = head;
+    head = head->next;
+    tail->next = head;
+    head->prev = tail;
+    free(temp);
+    return head;
+}
+
+ 
+
+void display(struct Node *head) {
+    if (!head) { printf("NULL\n"); return; }
+    struct Node *temp = head;
+    do {
+        printf("%d <-> ", temp->data);
+        temp = temp->next;
+    } while (temp != head);
+    printf("(Back to head)\n");
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    struct Node *head = createList(n);
+    head = insertAtBeginning(head);
+  
+    head = deleteAtBeginning(head);
+  
+    display(head);
+    return 0;
+}
